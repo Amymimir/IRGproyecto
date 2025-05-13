0e2ae9d299e84eaa210c838d8495c3beb66db3fb
Eclodest <jaimegonaja05@gmail.com>
2025-05-13 12:00:02 +0200

    hola

--- irg-backend/BBDD/db.js
+++ irg-backend/BBDD/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise'); // Usa mysql2 con promesas
 
 const pool = mysql.createPool({
   host: '127.0.0.1',
-  port: 3307, 
+  port: 3306, 
   user: 'root',              // Cambia si usas otro usuario
   password: '',              // O pon tu contraseña si tienes una
   database: 'cataoculta',
@@ -15,7 +15,7 @@ async function testConnection() {
   try {
     const connection = await pool.getConnection();
     console.log("Conexión exitosa a MySQL");
-    connection.release(); // Libera la conexión después de usarla
+    connection.release(); // Libera la conexión despuésn de usarla
   } catch (err) {
     console.error("Error al conectar con MySQL:", err.message);
     console.error("Detalles del error:", err); // Loguear todo el objeto del error
